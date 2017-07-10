af955ea8fb3b385556cb61cbfb16057c455932d1
techmmr <mmishra@grepruby.com>
2017-07-10 14:26:16 +0530

    Shell Application created, deployed on heroku
    Create README.md
    updated package.json
    index.js shifted from lib/ to root

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
